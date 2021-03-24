f40d27fc9e66ed154b89f5416605e9ab78fe6e7a
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2021-03-23 21:09:19 -0300

    Criacao do ALB e demais recursos

---
+++ src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,47 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.RemovalPolicy;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+
+public class Service01Stack extends Stack {
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01")
+                .serviceName("service-01")
+                .cluster(cluster)
+                .cpu(512)
+                .memoryLimitMiB(1024)
+                .desiredCount(2)
+                .listenerPort(8080)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("java_project_aws")
+                                .image(ContainerImage.fromRegistry("kalilventura/java_project_aws:latest"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "ServiceLogGroup")
+                                                .logGroupName("Service01")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service01")
+                                        .build()))
+                                .build()
+                )
+                .build();
+    }
+}
