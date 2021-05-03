417f0142c224e03fbd64190767d206cc7410cbab
kalilventura <kalilventur@gmail.com>
2021-05-03 16:29:40 -0300

    RDS

---
+++ src/main/java/com/myorg/RdsStack.java
@@ -0,0 +1,26 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.CfnParameter;
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ec2.*;
+
+public class RdsStack extends Stack {
+    public RdsStack(final Construct scope, final String id, Vpc vpc) {
+        this(scope, id, null, vpc);
+    }
+
+    public RdsStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
+        super(scope, id, props);
+
+        CfnParameter databasePassword = CfnParameter.Builder
+                .create(this, "databasePassword")
+                .type("String")
+                .description("RDS Instance password")
+                .build();
+
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
+    }
+}
