172fc9c0a6ef31f3c1ae95b090ba28c336275a95
kalilventura <kalilventur@hotmail.com>
2021-08-25 22:40:33 -0300

    Aplicacao para os eventos

---
+++ products_event_log/src/main/java/br/com/github/kalilventura/model/Envelope.java
@@ -0,0 +1,24 @@
+package br.com.github.kalilventura.model;
+
+import br.com.github.kalilventura.enums.EventType;
+
+public class Envelope {
+    private EventType eventType;
+    private String data;
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ products_event_log/src/main/java/br/com/github/kalilventura/repository/ProductEventLogRepository.java
@@ -0,0 +1,16 @@
+package br.com.github.kalilventura.repository;
+
+import br.com.github.kalilventura.model.ProductEventKey;
+import br.com.github.kalilventura.model.ProductEventLog;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+@EnableScan
+public interface ProductEventLogRepository extends CrudRepository <ProductEventLog, ProductEventKey>{
+
+    List<ProductEventLog> findAllByPk(String code);
+
+    List<ProductEventLog> findAllByPkAndSkStartsWith(String code, String eventType);
+}
